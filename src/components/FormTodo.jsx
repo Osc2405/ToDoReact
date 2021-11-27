4c52ad7c777fb4cc50113ac1f30ff74e8c631cc4
Osc2405 <orosero2405@gmail.com>
2021-11-27 18:38:30 -0500

    Componentes base

---
+++ src/components/FormTodo.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+
+const FormTodo = (props) => {
+    const [description, setDescription]=useState("");
+    return (
+        <form>
+            <div className="todo-list">
+                <div className="file-input">
+                    <input type="text" className="text" value={description} onChange={e => setDescription(e.target.value)} />
+                    <button className="button pink" disabled={description ? "":"disable"}> Add </button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default FormTodo
