02d4b179babeea02671453572a77eba7f683549e
Osc2405 <orosero2405@gmail.com>
2021-11-27 19:02:08 -0500

    Funcionalidad componentes

--- src/components/FormTodo.jsx
+++ src/components/FormTodo.jsx
@@ -2,8 +2,21 @@ import React, { useState } from 'react'
 
 const FormTodo = (props) => {
     const [description, setDescription]=useState("");
+    const {handleAddItem}=props;
+    const handleSubmit=e =>{
+        e.preventDefault();
+        handleAddItem({
+            done: false,
+            id: (+new Date()).toString(),
+            description
+        });
+        setDescription("");
+    }
+
+    
+    
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className="todo-list">
                 <div className="file-input">
                     <input type="text" className="text" value={description} onChange={e => setDescription(e.target.value)} />
